a9f77c1779ffb285c9cf18e85cf6c4b8c2871061
t.sato6 <t.sato6@nxtech.co.jp>
2024-12-12 11:06:29 +0900

    word change2

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 fun main() {
-    val name = "Kotlin_test"
+    val name = "Kotlin_test_Branch"
 //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
 // IntelliJ IDEA によるその修正案を確認できます。
     println("Hello, " + name + "!")
